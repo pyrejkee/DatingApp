9fbf640746b668662e2d2caae0e3f26f1f898266
Kiryl_Kiryanchykau <kiryl_kiryanchykau@epam.com>
2019-02-25 17:12:08 +0300

    Add interaction with photo

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -3,6 +3,7 @@ using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
+using AutoMapper;
 using DatingApp.API.Data;
 using DatingApp.API.Dtos;
 using DatingApp.API.Models;
@@ -18,9 +19,11 @@ namespace DatingApp.API.Controllers
     {
         private readonly IAuthRepository _repo;
         private readonly IConfiguration _config;
+        private readonly IMapper _mapper;
 
-        public AuthController(IAuthRepository repo, IConfiguration config)
+        public AuthController(IAuthRepository repo, IConfiguration config, IMapper mapper)
         {
+            _mapper = mapper;
             _repo = repo;
             _config = config;
         }
@@ -30,7 +33,7 @@ namespace DatingApp.API.Controllers
         {
             userForRegisterDto.UserName = userForRegisterDto.UserName.ToLower();
 
-            if(await _repo.userExists(userForRegisterDto.UserName))
+            if (await _repo.userExists(userForRegisterDto.UserName))
                 return BadRequest("Username already exists");
 
             var userToCreate = new User
@@ -48,7 +51,7 @@ namespace DatingApp.API.Controllers
         {
             var userFromRepo = await _repo.Login(userForLoginDto.UserName.ToLower(), userForLoginDto.Password);
 
-            if(userFromRepo == null)
+            if (userFromRepo == null)
                 return Unauthorized();
 
             var claims = new[]
@@ -71,9 +74,12 @@ namespace DatingApp.API.Controllers
 
             var token = tokenHandler.CreateToken(tokenDescriptor);
 
-            return Ok(new 
+            var user = _mapper.Map<UserForListDto>(userFromRepo);
+
+            return Ok(new
             {
-                token = tokenHandler.WriteToken(token)
+                token = tokenHandler.WriteToken(token),
+                user
             });
         }
     }
